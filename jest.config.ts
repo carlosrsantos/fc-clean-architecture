b1f676aa3a795987e0fbd8fd9e83fd0a3ba3459d
Carlos Rodrigues <crodrigsantos@gmail.com>
2026-07-27 19:37:53 -0300

    chore: ignorar testes na pasta /dir

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@
 export default {
   transform: {
     "^.+\.(t|j)sx?$": ["@swc/jest"],
-    
+
   },
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -156,9 +156,10 @@ export default {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/dist/"
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
